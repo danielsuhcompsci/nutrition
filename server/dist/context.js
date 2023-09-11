11deb8e7ba0075710f35329a431aa24a6743464a
danielsuhcompsci <danielsuh112@yahoo.com>
2023-09-11 13:48:53 -0500

    remove dist folder

--- server/dist/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = void 0;
-const db_1 = require("./db");
-// created for each request
-const createContext = () => ({
-    db: db_1.db,
-});
-exports.createContext = createContext;
