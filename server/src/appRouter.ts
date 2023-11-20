82e83aa77d04b413282f07c1dc3989563dc98a98
danielsuhcompsci <danielsuh112@yahoo.com>
2023-11-20 15:24:49 -0600

    added logs to workflow, router updates

--- server/src/appRouter.ts
+++ server/src/appRouter.ts
@@ -2,12 +2,12 @@ import { foodRouter } from "./routers/food";
 import { helloRouter } from "./routers/hello";
 import { publicProcedure, router, mergeRouters } from "./trpc";
 
-// export const appRouter = router({
-//   hello: helloRouter,
-//   food: foodRouter,
-// });
+export const appRouter = router({
+  hello: helloRouter,
+  food: foodRouter,
+});
 
-export const appRouter = mergeRouters(helloRouter, foodRouter);
+// export const appRouter = mergeRouters(helloRouter, foodRouter);
 
 // Export type router type signature,
 // NOT the router itself.
